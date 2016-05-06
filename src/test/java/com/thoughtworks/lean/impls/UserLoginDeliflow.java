f91ca2fe8d6f5f0f16dad25c3b300d8fcdb6b631
qmxie <qmxie@thoughtworks.com>
2016-05-06 13:43:28 +0800

    [#0] 以SharedWebDriver方式进行测试,节约测试时间

--- src/test/java/com/thoughtworks/lean/impls/UserLoginDeliflow.java
+++ src/test/java/com/thoughtworks/lean/impls/UserLoginDeliflow.java
@@ -1,22 +1,24 @@
 package com.thoughtworks.lean.impls;
 
-import cucumber.api.Scenario;
+import com.thoughtworks.lean.utils.SharedDriver;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 import static org.junit.Assert.assertTrue;
 
 /**
  * Created by qmxie on 5/5/16.
  */
-public class LoginDeliflow extends BaseUITest {
+public class UserLoginDeliflow {
 
+    private WebDriver driver;
 
     @Given("^I enter deliflow url$")
     public void enter_deliflow_url() {
-        driver = getWebDriver(scenario.getName());
+        driver = new SharedDriver();
         driver.navigate().to("http://121.42.193.129:9900/login");
     }
 
@@ -32,9 +34,4 @@ public class LoginDeliflow extends BaseUITest {
         assertTrue(driver.getTitle().contains("DeliFlow"));
     }
 
-
-    @cucumber.api.java.Before("@ui_test")
-    public void before_ui_test(Scenario scenario) {
-        this.scenario = scenario;
-    }
 }
