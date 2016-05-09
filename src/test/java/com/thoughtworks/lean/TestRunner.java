50121999c80bb49a8f5ea58d376e497ff12c273f
YongliuLi <ylli@thoughtworks.com>
2016-05-09 16:01:49 +0800

    添加cucumber报告输出

--- src/test/java/com/thoughtworks/lean/TestRunner.java
+++ src/test/java/com/thoughtworks/lean/TestRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/feature"})
+@CucumberOptions(features = {"src/test/resources/feature","html:target/cucumber","json:target/cucumber.json"})
 public class TestRunner {
 
 }
