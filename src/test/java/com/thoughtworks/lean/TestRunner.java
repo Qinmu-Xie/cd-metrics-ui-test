260923748e56b754fe130c814748fde94c75f7bc
YongliuLi <ylli@thoughtworks.com>
2016-05-09 16:07:42 +0800

    添加cucumber报告输出(配置错误 )

--- src/test/java/com/thoughtworks/lean/TestRunner.java
+++ src/test/java/com/thoughtworks/lean/TestRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/feature","html:target/cucumber","json:target/cucumber.json"})
+@CucumberOptions(features = {"src/test/resources/feature"},plugin = {"html:target/cucumber","json:target/cucumber.json"})
 public class TestRunner {
 
 }
